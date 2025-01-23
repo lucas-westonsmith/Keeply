6f7360566b27edf1351f08d9c104206fe6730674
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2025-01-23 20:03:39 +0100

    Fix migrations and reset database

--- db/migrate/20250120164559_add_super_list_to_lists.rb
+++ db/migrate/20250120164559_add_super_list_to_lists.rb
@@ -4,28 +4,18 @@ class AddSuperListToLists < ActiveRecord::Migration[7.0]
 
     reversible do |dir|
       dir.up do
-        # Assurez-vous qu'un utilisateur existe pour associer les super-listes
-        user = User.first || User.create!(
-          email: "default@example.com",
-          password: "password",
-          first_name: "Default",
-          last_name: "User",
-          date_of_birth: Date.new(1990, 1, 1)
-        )
-
         # Désactiver les validations et les callbacks pour cette migration
         SuperList.skip_callback(:create, :after, :create_default_lists)
 
-        # Créez les sur-listes par défaut sans validations
+        # Créez les sur-listes par défaut sans utilisateur associé
         super_lists = [
-          { title: "Home items", default: true, user: user },
-          { title: "Everyday life", default: true, user: user },
-          { title: "Administrative papers", default: true, user: user }
+          { title: "Home items", default: true },
+          { title: "Everyday life", default: true },
+          { title: "Administrative papers", default: true }
         ]
 
         super_lists.each do |attributes|
-          super_list = SuperList.new(attributes)
-          super_list.save!(validate: false) # Désactivation des validations
+          SuperList.create!(attributes.merge(user_id: nil)) # Assure-toi que `user_id` est nullable
         end
 
         # Réactiver les callbacks
