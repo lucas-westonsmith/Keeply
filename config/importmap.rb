2fe7eec6d3a6cd8353278ee1c9fd7a411b449f5c
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2025-01-29 17:53:03 +0100

    Fix importmap path to precompiled application.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # config/importmap.rb
 
 # Pin npm packages by running ./bin/importmap
-pin "application", to: "application.js" # Pour le fichier principal de l'application
+pin "application", to: "application-62ea37dc4286b8dc4dfa8219fd31fe3efdc4b71d2e1c1230b375b846164a3358.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js" # Pour Turbo
 pin "@hotwired/stimulus", to: "stimulus.min.js" # Pour Stimulus
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js" # Pour charger les contrôleurs Stimulus
