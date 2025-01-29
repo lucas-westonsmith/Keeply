9d6f397405136334146c9288e00781b3252113a2
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2025-01-29 23:38:26 +0100

    Fix Cloudinary avatar in navbar

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,16 +2,19 @@ module ItemsHelper
   def cloudinary_invoice_url(item)
     return unless item.invoice.attached?
 
+    # Déterminer l'environnement (production ou development)
+    env_folder = Rails.env.production? ? "production" : "development"
+
     # Récupérer la clé Cloudinary
     invoice_key = item.invoice.key
 
     # Récupérer la bonne version de l'asset depuis Cloudinary
     begin
-      asset_details = Cloudinary::Api.resource("development/#{invoice_key}", type: "upload", resource_type: "image")
+      asset_details = Cloudinary::Api.resource("#{env_folder}/#{invoice_key}", type: "upload", resource_type: "image")
       version = asset_details["version"]
-      "https://res.cloudinary.com/#{Cloudinary.config.cloud_name}/image/upload/v#{version}/development/#{invoice_key}.pdf"
+      "https://res.cloudinary.com/#{Cloudinary.config.cloud_name}/image/upload/v#{version}/#{env_folder}/#{invoice_key}.pdf"
     rescue StandardError => e
-      Rails.logger.error "Erreur Cloudinary: #{e.message}"
+      Rails.logger.error "Cloudinary Error: #{e.message}"
       nil
     end
   end
