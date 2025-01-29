fba3fce763aa0804f5365e485a8d90f3aa10a6c8
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2025-01-29 23:16:15 +0100

    Saved the day

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :item_lists, dependent: :destroy # Supprime les item_lists liés
+  has_many :item_lists, dependent: :destroy
   has_many :lists, through: :item_lists
   has_one_attached :photo
   has_one_attached :invoice
@@ -12,36 +12,21 @@ class Item < ApplicationRecord
 
   # Détermine l'image par défaut pour un item
   def default_image
-    # Vérifie si l'item appartient à une liste créée par défaut
     default_list = lists.find { |list| list.super_list&.default? }
 
-    # Retourne l'image spécifique à la liste si elle existe
     case default_list&.title
-    when 'Living room'
-      'living-room-2-default.avif'
-    when 'Kitchen'
-      'kitchen-2-default.avif'
-    when 'Bedroom'
-      'bedroom-2-default.avif'
-    when 'Clothing'
-      'clothing-2-default.avif'
-    when 'Supermarket'
-      'supermarket-2-default.avif'
-    when 'Restaurants / Cafés'
-      'restaurant-2-default.avif'
-    when 'Other expenses'
-      'other-expenses-2-default.avif'
-    when 'Subscriptions'
-      'subscriptions-2-default.avif'
-    when 'Taxes'
-      'taxes-2-default.avif'
-    when 'Payslips'
-      'payslips-2-default.avif'
-    when 'Insurance contracts'
-      'insurance-2-default.avif'
-    else
-      # Retourne l'image par défaut générale
-      'default-item.avif'
+    when 'Living room' then 'living-room-2-default.avif'
+    when 'Kitchen' then 'kitchen-2-default.avif'
+    when 'Bedroom' then 'bedroom-2-default.avif'
+    when 'Clothing' then 'clothing-2-default.avif'
+    when 'Supermarket' then 'supermarket-2-default.avif'
+    when 'Restaurants / Cafés' then 'restaurant-2-default.avif'
+    when 'Other expenses' then 'other-expenses-2-default.avif'
+    when 'Subscriptions' then 'subscriptions-2-default.avif'
+    when 'Taxes' then 'taxes-2-default.avif'
+    when 'Payslips' then 'payslips-2-default.avif'
+    when 'Insurance contracts' then 'insurance-2-default.avif'
+    else 'default-item.avif'
     end
   end
 end
