24ce32b31cb42a6e9a03a9e392124ed72ca7ad1b
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2025-01-29 18:42:02 +0100

    Correct importmap reference for application.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
-pin "application", to: "application.js", preload: true  # Chargement du fichier application.js principal
+pin "application", to: "https://www.keeply.tech/assets/application-62ea37dc4286b8dc4dfa8219fd31fe3efdc4b71d2e1c1230b375b846164a3358.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"  # Pour Turbo
 pin "@hotwired/stimulus", to: "stimulus.min.js"  # Pour Stimulus
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"  # Pour charger les contrôleurs Stimulus
