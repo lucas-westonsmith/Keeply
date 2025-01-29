82b023a6034ee9abfa390b736fc0bc24dcb49e57
Lucas Weston Smith <lucas.westonsmith@essec.edu>
2025-01-29 18:30:07 +0100

    Fix importmap and application.js path

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,9 @@
-# config/importmap.rb
+pin "application", to: "application.js", preload: true  # Chargement du fichier application.js principal
+pin "@hotwired/turbo-rails", to: "turbo.min.js"  # Pour Turbo
+pin "@hotwired/stimulus", to: "stimulus.min.js"  # Pour Stimulus
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"  # Pour charger les contrôleurs Stimulus
+pin_all_from "app/javascript/controllers", under: "controllers", preload: true  # Préchargement des contrôleurs Stimulus
 
-# Pin npm packages by running ./bin/importmap
-pin "application", preload: true # Pour charger l'application
-pin "@hotwired/turbo-rails", to: "turbo.min.js" # Pour Turbo
-pin "@hotwired/stimulus", to: "stimulus.min.js" # Pour Stimulus
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js" # Pour charger les contrôleurs Stimulus
-pin_all_from "app/javascript/controllers", under: "controllers", preload: true # Pour précharger tous les contrôleurs Stimulus
-
-# Pour Bootstrap et Popper.js, nécessaires pour les dropdowns et autres composants interactifs
+# Pour Bootstrap et Popper.js
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
